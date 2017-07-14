87d528bb99443524427bfa3712721159c5279f55
Captp2 <Captp2@users.noreply.github.com>
2017-07-14 12:11:28 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'Je vis 2hors',
+    'password' => '',
     'charset' => 'utf8',
 ];
